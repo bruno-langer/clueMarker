287319d30b81b0bea5af51391cf57e94b302dd79
Bruno Langer <bruno.langer@erzinger.com.br>
2020-09-28 15:32:50 -0300

    Updates

---
+++ precache-manifest.d9fbad021d56f4097db45e806b20e7c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60b23b7d6afb0c56146636346b48c4b",
+    "url": "/clueMarker/index.html"
+  },
+  {
+    "revision": "f8ef46d80d2197a0a47f",
+    "url": "/clueMarker/static/css/main.2e43fec3.chunk.css"
+  },
+  {
+    "revision": "1870fcf1de9fe93b4c92",
+    "url": "/clueMarker/static/js/2.b793defc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clueMarker/static/js/2.b793defc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ef46d80d2197a0a47f",
+    "url": "/clueMarker/static/js/main.b1fda204.chunk.js"
+  },
+  {
+    "revision": "98c0dc740d1b94e7dd48",
+    "url": "/clueMarker/static/js/runtime-main.dda0bafd.js"
+  },
+  {
+    "revision": "fbf2cfaa945ca9293d0b9212f772d027",
+    "url": "/clueMarker/static/media/train.fbf2cfaa.jpg"
+  }
+]);
