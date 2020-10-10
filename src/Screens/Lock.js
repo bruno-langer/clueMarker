72b179701d7dec35fe8b9506d5744131f8c9345e
Bruno Langer <bruno.langer@erzinger.com.br>
2020-10-10 10:21:24 -0300

    update

--- src/Screens/Lock.js
+++ src/Screens/Lock.js
@@ -1,7 +1,10 @@
 import React from 'react';
-
+import Secret from '../fotos/secret.png'
 const Lock = (props) => {
-    return (<h1>LOCK</h1>);
+    return (<div className={'container'}>
+        <img className={'lockIcon'} src={Secret}></img>
+        <button>RESETAR GAME</button>
+    </div>);
 }
 
 export default Lock;
